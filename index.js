914700dfbba2d90e92ff92c6da25931473c561c4
Aeon <119334575+anyzzio@users.noreply.github.com>
2023-12-02 18:01:55 +0100

    UI-changes

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 
 let playerWinsRound = 0;
 let computerWinsRound = 0;
+let tieRound = 0;
+
+const playerScore = document.querySelector("#scorePlayer");
+const pcScore = document.querySelector("#scorePc");
+const tieScore = document.querySelector("#scoreTie");
+
+const btnRock = document.querySelector("#btn1");
+const btnPaper = document.querySelector("#btn2");
+const btnScissors = document.querySelector("#btn3");
+
+const winner = document.querySelector("#announceWinner");
 
 
-function playRound(){
-    let playerSelection = (prompt("Choose your item: (ROCK) (PAPER) (SCISSORS)")).toUpperCase();
+function playRound(item){
+    let playerSelection = item;
     let computerSelection = getComputerChoice();
-
-
-    while(playerSelection !== "ROCK" && playerSelection !== "PAPER" && playerSelection !== "SCISSORS"){
-        console.log("Invalid choice! Please choose again.");
-        playerSelection = prompt("Choose your item: (ROCK) (PAPER) (SCISSORS)").toUpperCase();
-    }
 
     if(playerSelection === computerSelection){
         console.log("It`s a tie! Let`s play again.")
+        tieRound++;
     }else if(playerSelection === "ROCK" && computerSelection === "PAPER"){
         return computerWinsRound++;
     }else if(playerSelection === "PAPER" && computerSelection === "SCISSORS"){
@@ -26,22 +32,49 @@ function playRound(){
     }
 }
 
-function game(){
-    for(let i = 0;i < 5;i++){
-        playRound();
-    }
 
+function resetScores() {
+    playerWinsRound = 0;
+    computerWinsRound = 0;
+    tieRound = 0;
+    playerScore.textContent = playerWinsRound;
+    pcScore.textContent = computerWinsRound;
+    tieScore.textContent = tieRound;
+}
 
-    if(playerWinsRound > computerWinsRound){
-        console.log("Player Wins");
-    }else if(computerWinsRound > playerWinsRound){
-        console.log("Computer Wins");
-    }else{
-        console.log("Its a tie!")
+function updateScores() {
+    playerScore.textContent = playerWinsRound;
+    pcScore.textContent = computerWinsRound;
+    tieScore.textContent = tieRound;
+}
+
+function winnerAndNewRound() {
+    if (playerWinsRound === 5) {
+        winner.textContent = "Player";
+        resetScores();
+    } else if (computerWinsRound === 5) {
+        winner.textContent = "Computer";
+        resetScores();
     }
 }
 
+btnRock.addEventListener("click", () => {
+    playRound("ROCK");
+    updateScores();
+    winnerAndNewRound();
+});
 
+btnPaper.addEventListener("click", () => {
+    playRound("PAPER");
+    updateScores();
+    winnerAndNewRound();
+});
+
+btnScissors.addEventListener("click", () => {
+    playRound("SCISSORS");
+    updateScores();
+    winnerAndNewRound();
+});
 
 function randomizer(){
     let random = Math.floor((Math.random() * 3)+1);
@@ -59,4 +92,4 @@ function getComputerChoice(){
     }
 }
 
-game();
+
